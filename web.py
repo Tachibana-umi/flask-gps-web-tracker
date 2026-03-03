dee6bc7a5b9508d408a2528f15f4a4d46d553be9
Tachibana Umi <2305797612@qq.com>
2026-03-03 17:30:17 +0800

    前后端传输完成，位置解析完成

--- web.py
+++ web.py
@@ -1,15 +1,34 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Welcome to the Home Page!<h1>This is a heading</h1>'
+def index():
+    return app.send_static_file('index.html')
 
+#url传递参数
+@app.route('/info', methods=['POST'])
+def info():
+    #GET请求参数
+    # pwd = request.args.get('pwd')
+    # print(pwd)
+
+    #POST请求参数
+    info = request.get_json()
+    print(info)
+
+    return jsonify({"status": "success", "message": "服务器已收到数据"})
+
+    #JSON数据
+    # print(request.json, type(request.json))
+
+    
+#变量传递
 # @app.route('/<name>')
 # def show_info(name):
 #     return f'<h1>Hello, {name}!</h1>'
 
+#重定向
 # @app.route('/admin')
 # def admin():
 #     return redirect(url_for('home'))
